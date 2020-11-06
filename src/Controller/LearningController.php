0315030ebc13cb33a8e151051a646766fd680b98
Lea Dalibard <leadalibard@gmail.com>
2020-11-06 17:16:13 +0100

    Saving last updates

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Name;
-use App\Form\NameType;
 
 //use Doctrine\DBAL\Types\TextType;
 //use http\Env\Request;
@@ -13,20 +12,70 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 
 class LearningController extends AbstractController
 {
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
     /**
-     * @Route("/about-me")
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+
+        return $this->render('learning/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+
+    /**
+     * @Route("/about-me", name="aboutMe")
+     */
+
+    public function aboutMe(): Response
+    {
+        return $this->render('learning/about-me.html.twig', [
+            'name' => $name,
+        ]);
+    }
+
+    /**
+     * @Route("/show-my-name", name="show-my-name")
      */
 
     public function showMyName(): Response
     {
         $name = 'Unknown';
-        return $this->render('learning/about-me.html.twig', [
+        return $this->render('learning/show-my-name.html.twig', [
             'name' => $name,
         ]);
     }
+
+    /**
+     * @Route("/changemyname", name="changemyname", methods="GET")
+     */
+
+    public function changeMyName(Request $request): Response
+    {
+        $name=$request->query->get("name");
+
+        $this->session->set('name', $name);
+
+        return $this->render('learning/change-my-name.html.twig', [
+            "name" =>  $this->session->get('name'),
+        ]);
+    }
+
+
+    //(if(session->get('name)) {} else {}
+//change to POST, POST['name']
 
     //public function new(Request $request)
     //    {
