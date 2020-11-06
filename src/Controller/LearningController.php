c68fd5eaa686675453a411c18af263c0a15649a4
Lea Dalibard <leadalibard@gmail.com>
2020-11-06 12:30:20 +0100

    Saving before lunch (doing form following symfony documentation)

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Name;
+//use Doctrine\DBAL\Types\TextType;
+//use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LearningController extends AbstractController
@@ -12,17 +18,28 @@ class LearningController extends AbstractController
      * @Route("/about-me")
      */
 
-    public function showMyName():Response
-    {$name='Unknown';
+    public function showMyName(): Response
+    {
+        $name = 'Unknown';
         return $this->render('learning/about-me.html.twig', [
             'name' => $name,
         ]);
     }
 
-    public function changeMyName():RedirectResponse
+    public function new( Request $request)
     {
-        return $this->redirectToRoute('index');
+        $name = new Name();
+
+        $form = $this->createFormBuilder($name)
+            ->add('name', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Change Name'])
+            ->getForm();
     }
+
+    //  public function changeMyName():RedirectResponse
+    //    {
+    //        return $this->redirectToRoute('index');
+    //    }
 
 
 }
